e7d256fc34c13cd51dfb22ffbb5cff8615f23fa1
LeeCombs <leecombs_30@hotmail.com>
2017-12-14 17:20:55 -0800

    BattleSceneTest layout

--- SimTests/BattleSceneTest.cs
+++ SimTests/BattleSceneTest.cs
@@ -16,29 +16,69 @@ namespace SimTests
             MonsterManager.LoadContent();
         }
 
-        [TestMethod]
-        public void SceneCreationTest()
-        {
-            // Test Defaults
-            BattleScene scene = new BattleScene();
 
-            Assert.AreEqual(0, scene.X);
-            Assert.AreEqual(0, scene.Y);
-            Assert.AreEqual(0, scene.GetAllTargets().Length);
-            Assert.IsNull(scene.GetAnySingleTarget());
-            Assert.IsNull(scene.GetFrontRowTarget());
+        [TestMethod]
+        public void ActionTest()
+        {
+            // Ensure Action struct behaves properly and things remain within bounds
+        }
+        
+        [TestMethod]
+        public void PopulateSceneTest()
+        {
+            // Ensure a valid monster list is populated as normal
+            // Ensure a too-long list doesn't overfill the scene
+            // Do this for A, B, and C-type scenes
         }
 
         [TestMethod]
-        public void SceneATest()
+        public void ClearSceneTest()
         {
+            // Populate a scene with a valid monster list
+            // Ensure monsters are cleared as expected
+        }
+
+        [TestMethod]
+        public void GetMonsterActionsTest()
+        {
+            // Populate a scene with a valid monster list
+            // Ensure a list of monster actions are properly generated and returned
+            // This one sounds like a pain in the ass
+        }
+
+
+        [TestMethod]
+        public void GetAllTargetsTest()
+        {
+            // Populate a scene with a valid monster list
+            // Ensure all active/non-null targets are returned appropriately
+        }
+
+        [TestMethod]
+        public void GetAnySingleTargetTest()
+        {
+            // Populate a scene with a valid monster list
+            // Ensure only a monster from the scene is returned
+            // Ensure that a monster is actually returned if there is one
+            // Ensure nothing is returned if no monsters exist
+        }
+
+        [TestMethod]
+        public void SceneA_GetFrontRowTargetTest()
+        {
+            // Populate a scene with a valid monster list
+            // Ensure only front-row monsters are returned
+            // Do this for 4, 3, 2, and 1 column-deep scenes
+            // Do this for A, B, and C-type scenes
+            // Ensure null is returned or an exception is thrown if no monsters exist
+
             // Setup
             BattleScene scene = new BattleScene(type: "A");
             List<string> monNames;
             List<Monster> monList = new List<Monster>();
 
             // Add 8 Monsters to the scene
-            monNames = new List<string>{ "Balloon", "Balloon", "Changer", "Changer", "DeadHead", "DeadHead", "Eagle", "Eagle" };
+            monNames = new List<string> { "Balloon", "Balloon", "Changer", "Changer", "DeadHead", "DeadHead", "Eagle", "Eagle" };
             foreach (string name in monNames)
                 monList.Add(MonsterManager.GetMonsterByName(name));
             scene.PopulateScene(monList);
@@ -80,5 +120,37 @@ namespace SimTests
             }
         }
 
+        [TestMethod]
+        public void SceneB_GetFrontRowTargetTest()
+        {
+            // Populate a scene with a valid monster list
+            // Ensure only front-row monsters are returned
+            // Do this for 4, 3, 2, and 1 column-deep scenes
+            // Do this for A, B, and C-type scenes
+            // Ensure null is returned or an exception is thrown if no monsters exist
+        }
+
+        [TestMethod]
+        public void SceneC_GetFrontRowTargetTest()
+        {
+            // Populate a scene with a valid monster list
+            // Ensure only front-row monsters are returned
+            // Do this for 4, 3, 2, and 1 column-deep scenes
+            // Do this for A, B, and C-type scenes
+            // Ensure null is returned or an exception is thrown if no monsters exist
+        }
+
+        [TestMethod]
+        public void SceneCreationTest()
+        {
+            // Test Defaults
+            BattleScene scene = new BattleScene();
+
+            Assert.AreEqual(0, scene.X);
+            Assert.AreEqual(0, scene.Y);
+            Assert.AreEqual(0, scene.GetAllTargets().Length);
+            Assert.IsNull(scene.GetAnySingleTarget());
+            Assert.IsNull(scene.GetFrontRowTarget());
+        }
     }
 }
