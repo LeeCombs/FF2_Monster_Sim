3f3a59965c7577573c7e91b8b4f3e504479cf9b1
HellaBored <leecombs_30@hotmail.com>
2018-02-06 20:30:00 -0800

    StatusSprite first step
    StatusSprite:
    - Multiple status textures
    - Basic animation
    
    SpriteManager:
    - Might want it?
    
    Content:
    - Silence -> Mute
    
    First Monogame sprite animation!

---
+++ FF2_Monster_Sim/SpriteManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FF2_Monster_Sim
+{
+    class SpriteManager
+    {
+        public SpriteManager()
+        {
+            //
+        }
+
+        //////////////
+        // Monogame //
+        //////////////
+
+        public void Initialize()
+        {
+            //
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            //
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            //
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //
+        }
+
+        /////////////
+        // Publics //
+        /////////////
+
+        /////////////
+        // Helpers //
+        /////////////
+
+    }
+}
